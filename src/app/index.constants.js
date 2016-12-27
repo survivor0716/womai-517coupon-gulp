166d73093fe4f92ecf2bc5e5995424f501cc6632
Survivor <dongchen1022@gmail.com>
2016-12-27 16:18:27 +0800

    12月27日备份

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('womai517couponGulp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
